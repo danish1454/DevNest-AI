24e5be450b10703973eb000ff3b3a44d349cf61d
Danish Mahajan <142664220+danish1454@users.noreply.github.com>
2025-07-10 00:53:22 +0530

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
  import React from 'react'
  import { Route, BrowserRouter, Routes } from 'react-router-dom'
- import Login from '../screen/login'
+ import Login from '../screen/Login'
  import Register from '../screen/Register'
 import Home from '../screen/Home'
 import Project from '../screen/Project'
